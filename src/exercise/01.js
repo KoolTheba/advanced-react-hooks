267f3b25b4fa44f7deba29d6f0c0286833bc058a
KoolTheba <gomez.teba@gmail.com>
2023-12-27 18:00:52 +0100

    feat: completed extra ex.1 traditional dispatch object

--- src/exercise/01.js
+++ src/exercise/01.js
@@ -1,36 +1,57 @@
 // useReducer: simple Counter
 import * as React from 'react'
 
-// 1.
+// 1. accept the step as the action
 // const countReducer = (count, step) => count + step;
 
 // 2. simulate setState with an object
 // const countReducer = (state, newState) => ({...state, ...newState})
 
 // 3. simulate setState with an object OR function
-const countReducer = (state, action) => ({
-  ...state, 
-  ...(typeof action === 'function' ? action(state) : action)
-})
+// const countReducer = (state, action) => ({
+//   ...state, 
+//   ...(typeof action === 'function' ? action(state) : action)
+// })
+
+// 4. traditional dispatch object with a type and switch statement
+const countReducer = (state, action) => {
+  const {type, step} = action;
+  switch (type) {
+    case 'INCREMENT':
+        return {
+            count: state.count + step
+        }
+    default:
+        return state
+}
+}
 
 function Counter({initialCount = 0, step = 2}) {
-  // 1.
+  // 1. accept the step as the action
   // const [count, changeCount] = React.useReducer(countReducer, initialCount)
   // const increment = () => changeCount(step)
 
-  // 2. Simulate setState with an object OR function
+  // 2. simulate setState with an object
   // const [state, setState] = React.useReducer(countReducer, {
   //   count: initialCount,
   // })
   // const {count} = state
   // const increment = () => setState({count: count + step})
 
-  // 3. 
-  const [state, setState] = React.useReducer(countReducer, {
+  // 3. Simulate setState with an object OR function
+  // const [state, setState] = React.useReducer(countReducer, {
+  //   count: initialCount,
+  // })
+  // const {count} = state
+  // const increment = () => setState(currentState => ({count: currentState.count + step}))
+  // return <button onClick={increment}>{count}</button>
+
+  // 4. traditional dispatch object with a type and switch statement
+  const [state, dispatch] = React.useReducer(countReducer, {
     count: initialCount,
   })
   const {count} = state
-  const increment = () => setState(currentState => ({count: currentState.count + step}))
+  const increment = () => dispatch({type: 'INCREMENT', step})
   return <button onClick={increment}>{count}</button>
 }
 
