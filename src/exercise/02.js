f3da26bbe5cee89f20e244938e85c639d10888b8
KoolTheba <gomez.teba@gmail.com>
2024-01-06 19:45:22 +0100

    feat: refactor for a safe dispatch when unmount

--- src/exercise/02.js
+++ src/exercise/02.js
@@ -9,6 +9,23 @@ import {
   PokemonErrorBoundary,
 } from '../pokemon'
 
+function useSafeDispatch (dispatch){
+  const mountedRef = React.useRef(false)
+
+  React.useEffect(() => {
+    mountedRef.current = true
+    return () => {
+      mountedRef.current = false
+    }
+  }, [])
+
+  return React.useCallback((...args) => {
+    if(mountedRef.current){
+      dispatch(...args)
+    }
+  }, [dispatch])
+}
+
 function asyncReducer(state, action) {
   switch (action.type) {
     case 'pending': {
@@ -27,12 +44,14 @@ function asyncReducer(state, action) {
 }
 
   function useAsync(initialState) {
-    const [state, dispatch] = React.useReducer(asyncReducer, {
+    const [state, unsafeDispatch] = React.useReducer(asyncReducer, {
       status: 'idle',
       data: null,
       error: null,
       ...initialState
     })
+
+    const dispatch = useSafeDispatch(unsafeDispatch)
 
     const run = React.useCallback((promise) => {
       dispatch({type: 'pending'})
@@ -44,7 +63,7 @@ function asyncReducer(state, action) {
           dispatch({type: 'rejected', error})
         },
       )
-    }, [])
+    }, [dispatch])
 
     return {...state, run};
   }
